df81dfbd3cfb9c52d74935817135e2e39362fc7e
venticx <venticx@gmail.com>
2025-04-05 18:22:45 +0200

    added instructions and inital program and service file

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
-# adafruit-sharp-memory-display
+# Adafruit Sharp Memory Display
+
 Python program to render linux /dev/fb0 framebuffer on adafruit 400x240 sharp memory display
+
+## Wiring
+
+| Display | Rasperri Pi   |
+| ------- | ------------- |
+| VIN     | 3.3V          |
+| 3v3     | Not connected |
+| GND     | GND           |
+| CLK     | 11 SCLK       |
+| DI      | 10 MOSI       |
+| CS      | 23            |
+
+## Preparation
+
+### Enable SPI
+
+- sudo raspi-config
+- select Interface Options
+- select SPI
+- confirm by selecting yes
+
+### Switch KMS to FKMS
+
+- open /boot/firmware/config.txt
+- change line "dtoverlay=vc4-kms-v3d" to "vc4-fkms-v3d"
+
+### Install Adafruit Sharp Memory Display Library
+
+```python
+pip3 install adafruit-circuitpython-sharpmemorydisplay
+
+```
+
+## Run Program
+
+```
+sudo path_to_venv/bin/python3 __main__.py
+```
+
+## Add System Service To Start On Boot
+
+```
+cp sharp_display.service /lib/systemd/system/
+```
+
+- open /lib/systemd/system/sharp_display.service
+- change ExecStart python3 path to path_to_venv/bin/python3
+- change ExecStart executable path to the localy cloned repo
+
+```
+sudo systemctl daemon-reload
+sudo systemctl enable sharp_display.service
+sudo reboot
+```
+
+# Sources
+
+## Display
+
+https://learn.adafruit.com/adafruit-sharp-memory-display-breakout/python-usage
+
+https://github.com/adafruit/Adafruit_CircuitPython_SharpMemoryDisplay
+
+## Service
+
+https://github.com/ian-antking/cardkb
---
+++ __main__.py
@@ -0,0 +1,69 @@
+import board
+import busio
+import digitalio
+import adafruit_sharpmemorydisplay
+import time
+import numpy as np
+import mmap
+
+# Framebuffer parameters
+BUFFER_PATH = "/dev/fb0"
+BUFFER_WIDTH = 720
+BUFFER_HEIGHT = 480
+BUFFER_BIT_DEPTH = 16
+
+# Adafrui sharp memory display parameters
+WIDTH = 400
+HEIGHT = 240
+
+# Calculated Parameters 
+BYTES_PER_PIXEL = BUFFER_BIT_DEPTH // 8
+BUFFER_SIZE = BUFFER_WIDTH * BUFFER_HEIGHT * BYTES_PER_PIXEL
+ys = np.linspace(0, BUFFER_HEIGHT - 1, HEIGHT, dtype=np.uint16)
+xs = np.linspace(0, BUFFER_WIDTH - 1, WIDTH, dtype=np.uint16)
+grid_y, grid_x = np.meshgrid(ys, xs, indexing='ij')
+
+# Board pin mapping
+spi = busio.SPI(board.SCK, MOSI=board.MOSI)
+scs = digitalio.DigitalInOut(board.D23)  # inverted chip select
+
+display = adafruit_sharpmemorydisplay.SharpMemoryDisplay(spi, scs, WIDTH, HEIGHT)
+
+
+def clear():
+    display.fill(1)
+    display.show()
+
+def refresh():
+    # Read framebuffer from path
+    with open(BUFFER_PATH, "rb") as fb:
+        fb_mem = mmap.mmap(fb.fileno(), BUFFER_SIZE, access=mmap.ACCESS_READ)
+        arr = np.frombuffer(fb_mem, dtype=np.uint16).reshape((BUFFER_HEIGHT, BUFFER_WIDTH))
+
+        # Downscaling
+        downscaled = arr[grid_y, grid_x]
+
+        # Pack bits row-by-row (axis=1 preserves alignment)
+        packed = np.packbits(downscaled, axis=1, bitorder='big')
+        byte_array = packed.flatten().tobytes()
+
+        # Assign framebuffer byte array to display buffer
+        display.buffer = byte_array
+
+    fb.close()
+
+    # Display image
+    display.show()
+
+# Main program loop
+clear()
+try:
+    while True:
+        refresh()
+        time.sleep(1/30)
+except Exception as e:
+    print(e) 
+except KeyboardInterrupt:
+    print("program was interrupted by user.")
+finally:
+    clear()
